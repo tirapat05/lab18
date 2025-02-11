be4925ed60da1791f1a30db976fcde28909d4b3c
tirapat05 <tirapat05.05@gmail.com>
2025-02-11 23:09:28 +0700

    Fist version

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1, Rect r2){
+double x_overlap = max(0.0, min(r1.x + r1.w, r2.x + r2.w) - max(r1.x, r2.x));
+    double y_overlap = max(0.0, min(r1.y, r2.y) - max(r1.y - r1.h, r2.y - r2.h));
+    
+    return x_overlap * y_overlap;
 }
